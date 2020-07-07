c472d84d581dd98e7e31cb685eef889eba0e51ad
Tero Lamminpaa <tero@pop-os.localdomain>
2020-07-07 13:15:50 +0300

    Accessibility information is now included in plugin output

--- default-templates/components/service_location_service_channel/addresses.php
+++ default-templates/components/service_location_service_channel/addresses.php
@@ -19,6 +19,26 @@
       $mapUrl = $serviceId ? "https://www.suomi.fi/kartta/palvelupaikat/$serviceId?sl=$serviceChannelId" : null;
       $routeUrl = $latitude && $longitude ? "https://www.suomi.fi/kartta/reitit?to.lat=$latitude&to.lon=$longitude" : null;
 
+      /**
+       * PTV accessibility information
+       */
+      if ($address['entrances']) {
+        foreach ($address['entrances'] as $entrance) {
+          foreach ($entrance['accessibilitySentences'] as $accessibilitySentence) {
+            $sentenceGroups = $accessibilitySentence['sentenceGroup'];
+            $key = array_search('fi', array_column($sentenceGroups, 'language'));
+            echo "<h4>" . $sentenceGroups[$key]['value'] . "</h4>";
+            
+            foreach ($accessibilitySentence['sentences'] as $sentences) {
+              $sentence = $sentences['sentence'];
+              $key = array_search('fi', array_column($sentence, 'language'));
+              echo "<p>" . $sentence[$key]['value'] . "</p>";
+            }
+          }
+        }
+        echo "<br/>";
+      }
+
       echo getLocalizedValue($streetAddress["street"], $data->language) . " " . $streetAddress["streetNumber"];
       echo "<br/>";
       echo $streetAddress["postalCode"] . " " . getLocalizedValue($streetAddress["postOffice"], $data->language);
