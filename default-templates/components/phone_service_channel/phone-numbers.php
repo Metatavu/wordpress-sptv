79784e75f27fb27ef9d55478c75837ce98532eb7
Daniilmass <daniil.kings@gmail.com>
2020-08-27 17:29:41 +0300

    Update default-templates/components/phone_service_channel/phone-numbers.php
    Co-authored-by: Tero Lamminpää <56538990+terolamminpaa@users.noreply.github.com>

--- default-templates/components/phone_service_channel/phone-numbers.php
+++ default-templates/components/phone_service_channel/phone-numbers.php
@@ -2,7 +2,7 @@
   require_once(__DIR__ . '/../common.php');
 
   $serviceChannel = $data->serviceChannel;
-  $phoneNumbers = $serviceChannel ["phoneNumbers"];
+  $phoneNumbers = $serviceChannel["phoneNumbers"];
 
 
   if (!$phoneNumbers) {
