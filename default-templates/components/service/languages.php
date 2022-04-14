0ce0d957d9f4c442a407b6e2d70a281b76e45b8d
Simeon Platonov <simeon.platonov@gmail.com>
2022-04-14 16:15:21 +0300

    Added languages component

--- default-templates/components/service/languages.php
+++ default-templates/components/service/languages.php
@@ -1,5 +1,8 @@
 <?php
   require_once(__DIR__ . '/../common.php');
-  
-  var_dump($data);
+  $languages = $data->service["languages"];
+  echo "<h3>Kielet, joilla palvelu on saatavilla</h3>";
+  foreach ($languages as $language) {
+    echo "<span>$language</span><br/>";
+  }
 ?>
