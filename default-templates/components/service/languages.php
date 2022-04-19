cbf256b09d526a8d3cf92aeb3fd5d7b63b522f74
Simeon Platonov <simeon.platonov@gmail.com>
2022-04-19 12:14:31 +0300

    Missing semicolon

--- default-templates/components/service/languages.php
+++ default-templates/components/service/languages.php
@@ -1,7 +1,7 @@
 <?php
   require_once(__DIR__ . '/../common.php');
   $languages = $data->service["languages"];
-  $languageText = __("AvailableLanguages", "sptv")
+  $languageText = __("AvailableLanguages", "sptv");
   echo "<h3>$languageText</h3>";
   foreach ($languages as $language) {
     $localizedLanguage = getLocalizedLanguageName($language);
