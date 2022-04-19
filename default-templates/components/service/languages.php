4d2f65a7a94a7dbc06fbce403b40522704ed2685
Simeon Platonov <simeon.platonov@gmail.com>
2022-04-19 07:54:08 +0300

    Localized languages-component text

--- default-templates/components/service/languages.php
+++ default-templates/components/service/languages.php
@@ -1,7 +1,8 @@
 <?php
   require_once(__DIR__ . '/../common.php');
   $languages = $data->service["languages"];
-  echo "<h3>Kielet, joilla palvelu on saatavilla</h3>";
+  $languageText = __("AvailableLanguages", "sptv")
+  echo "<h3>$languageText</h3>";
   foreach ($languages as $language) {
     $localizedLanguage = getLocalizedLanguageName($language);
     echo "<span>$localizedLanguage</span><br/>";
