9f0745d8e686b9d566974232e72abedabcb1c5e3
platonovsimeon <simeon.platonov@gmail.com>
2022-06-14 12:25:03 +0300

    Added styles to accesibility

--- default-templates/components/service_location_service_channel/accessibility.php
+++ default-templates/components/service_location_service_channel/accessibility.php
@@ -19,21 +19,25 @@
        * PTV accessibility information
        */
       if ($address['entrances']) {
+        echo "<div class='accessibility-sentences'>"
         echo "<h2>$blockHeader</h2>";
         foreach ($address['entrances'] as $entrance) {
           foreach ($entrance['accessibilitySentences'] as $accessibilitySentence) {
             $sentenceGroups = $accessibilitySentence['sentenceGroup'];
             $key = array_search('fi', array_column($sentenceGroups, 'language'));
-            echo "<h4>" . $sentenceGroups[$key]['value'] . "</h4>";
-            
+            echo "<div class='accessibility-sentence'>"
+            echo "<button type='button' class='button-text closed'>" . $sentenceGroups[$key]['value'] . "</button>";
+            echo "<div class='accessibility-content'>";
             foreach ($accessibilitySentence['sentences'] as $sentences) {
               $sentence = $sentences['sentence'];
               $key = array_search('fi', array_column($sentence, 'language'));
               echo "<p>" . $sentence[$key]['value'] . "</p>";
             }
+            echo "</div>";
+            echo "</div>";
           }
         }
-        echo "<br/>";
+        echo "</div>";
       }
     }
     
